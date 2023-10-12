5b745c52c70f9dc8c0ed2337589141d52bc53aac
Ed Rial <eddrial@hotmail.com>
2023-10-12 11:06:03 +0200

    Changed import method?

--- tests/test_demo1.py
+++ tests/test_demo1.py
@@ -1,12 +1,12 @@
 #   import unittest
 import pytest
-import undulator_analysis_hzb.demo1 as demo1
+#import undulator_analysis_hzb.demo1 as demo1
+from undulator_analysis_hzb import demo1
 
 
 class TestDemo():
     def test_one(self):
         inp = 1
-        
         assert demo1.add_one(inp) == 2
         
     def test_two(self):
