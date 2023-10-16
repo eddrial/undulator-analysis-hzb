f0369aece5905318e7e0e38cc1303d338b0fe2db
Ed Rial <eddrial@hotmail.com>
2023-10-16 12:34:27 +0200

    First test for data_io

--- sandbox/in_out_sandbox.py
+++ sandbox/in_out_sandbox.py
@@ -5,12 +5,12 @@ Created on Oct 16, 2023
 '''
 import importlib.resources
 import numpy as np
-from undulator_analysis_hzb import data_io as io
+from undulator_analysis_hzb import data_io as dio
 
 if __name__ == '__main__':
     file_path = importlib.resources.files('undulator_analysis_hzb').joinpath('../../tests/resources/MAG1221.DVM')
     
-    io_object = io.data_io(file_path)
+    io_object = dio.data_io(file_path)
     
     with open(file_path) as f:
         a = np.genfromtxt(f)
---
+++ tests/test_data_io.py
@@ -0,0 +1,18 @@
+'''
+Created on Oct 16, 2023
+
+@author: oqb
+'''
+#   import unittest
+import pytest
+#import undulator_analysis_hzb.demo1 as demo1
+from undulator_analysis_hzb import data_io as dio
+import importlib.resources
+
+
+class TestConstructor():
+    
+    def test_one(self):
+        file_path = importlib.resources.files('undulator_analysis_hzb').joinpath('../../tests/resources/MAG1221.DVM')
+        assert dio.data_io(file_path).file_type == 'DVM'
+    
