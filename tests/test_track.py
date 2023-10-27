cfbe395f4df7baaec222f4b6150526f17e643f20
Ed Rial <eddrial@hotmail.com>
2023-10-27 15:19:30 +0200

    test track testing same as data_io

--- tests/test_track.py
+++ tests/test_track.py
@@ -21,47 +21,12 @@ class TestConstructor():
     def test_created(self, my_track):
         assert my_track.track_number == '0001'
         
-class TestTrackType():
-#    a = trk.track('0001')
-#    file_path = importlib.resources.files('undulator_analysis_hzb').joinpath('../../tests/resources/MAG1221.DVM')
-#    a.load_dvm_data(file_path)
+class TestTrackLoading():
+
     def test_load_dvm_data_file_type(self,my_track):
         
         assert my_track.file_type == 'DVM'
-'''        
-    def test_dat(self):
-        file_path = importlib.resources.files('undulator_analysis_hzb').joinpath('../../tests/resources/HP-FIELD1221.DAT')
-        assert dio.data_io(file_path).file_type == 'DAT'
+        
+    def test_open_dvm(self, my_track):
+        assert my_track.dvm_data.shape == (5150,3)
 
-class TestOpen():
-    
-    def test_open_dvm(self):
-        file_path = importlib.resources.files('undulator_analysis_hzb').joinpath('../../tests/resources/MAG1221.DVM')
-        file_object = dio.data_io(file_path)
-        assert file_object.open().shape == (5150,3)
-        
-    def test_self_dvm(self):
-        file_path = importlib.resources.files('undulator_analysis_hzb').joinpath('../../tests/resources/MAG1221.DVM')
-        file_object = dio.data_io(file_path)
-        file_object.open()
-        assert file_object.dvm_data.shape == (5150,3)
-        
-    def test_open_dat(self):
-        file_path = importlib.resources.files('undulator_analysis_hzb').joinpath('../../tests/resources/HP-FIELD1221.DAT')
-        file_object = dio.data_io(file_path)
-        assert file_object.open().shape == (5148,7)
-        
-    def test_self_dat(self):
-        file_path = importlib.resources.files('undulator_analysis_hzb').joinpath('../../tests/resources/HP-FIELD1221.DAT')
-        file_object = dio.data_io(file_path)
-        file_object.open()
-        assert file_object.proc_data.shape == (5148,7)
-        
-    
-    def test_self_xy(self):
-        file_path = importlib.resources.files('undulator_analysis_hzb').joinpath('../../tests/resources/HP-FIELD1221.DAT')
-        file_object = dio.data_io(file_path)
-        file_object.open()
-        #assert file_object.yz == np.array([-5.,0.])
-        np.testing.assert_array_equal(file_object.yz, np.array([0,-5]))'''
-        
