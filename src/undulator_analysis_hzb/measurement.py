6524cacec1318f4aea292d3b84b9082df034f419
Ed Rial <eddrial@hotmail.com>
2023-11-10 12:52:56 +0100

    Update to read logfile

--- src/undulator_analysis_hzb/measurement.py
+++ src/undulator_analysis_hzb/measurement.py
@@ -6,6 +6,7 @@ Created on Oct 16, 2023
 
 import numpy as np
 import undulator_analysis_hzb.track as trk
+import datetime as dt
 
 class measurement(object):
     '''
@@ -27,7 +28,7 @@ class measurement(object):
 class granite_bank_measurement(measurement):
     def __init__(self, measurement_name):
         super(granite_bank_measurement,self).__init__(measurement_name)
-    
+        self.name = measurement_name
     @classmethod
     def convert_to_granite_bank_measurement(cls,obj):
         obj.__class__ = granite_bank_measurement
@@ -35,6 +36,46 @@ class granite_bank_measurement(measurement):
     
     def read_logfile_metadata(self):
         f = open(self.logfile, 'r')
-        self.loglines = f.readlines()
-        print ('log data read into self.loglines')
+        loglines = f.readlines()
+        print ('log data read into loglines')
         
+        for line in range(len(loglines)):
+            if loglines[line][0:4] == 'Date':
+                self.measurement_timestamp = dt.datetime.strptime(loglines[line].split()[1] +
+                                                                  ' ' +
+                                                                  loglines[line].split()[2],'%d-%b-%y %H:%M:%S')
+            
+            if loglines[line].split()[0] == 'Operator:':
+                self.operator = loglines[line].split()[1]
+            
+            if loglines[line][0:17] == 'X AXIS  Parameter':
+                self.x_start = float(loglines[line+1].split()[-1])
+                self.x_end = float(loglines[line+2].split()[-1])
+                self.x_step = float(loglines[line+3].split()[-1])
+                self.x_velocity = float(loglines[line+5].split()[-1])
+                self.x_return_velocity = float(loglines[line+6].split()[-1])
+                self.x_unit = 'mm'
+            
+            if loglines[line][0:17] == 'Y AXIS  Parameter':
+                self.y_start = float(loglines[line+1].split()[-1])
+                self.y_end = float(loglines[line+2].split()[-1])
+                self.y_step = float(loglines[line+3].split()[-1])
+                self.y_velocity = float(loglines[line+5].split()[-1])
+                self.y_return_velocity = float(loglines[line+6].split()[-1])
+                self.y_unit = 'mm'
+                
+            if loglines[line][0:17] == 'Z AXIS  Parameter':
+                self.z_start = float(loglines[line+1].split()[-1])
+                self.z_end = float(loglines[line+2].split()[-1])
+                self.z_step = float(loglines[line+3].split()[-1])
+                self.z_velocity = float(loglines[line+5].split()[-1])
+                self.z_return_velocity = float(loglines[line+6].split()[-1])
+                self.z_unit = 'mm'
+                
+            if loglines[line][0:16] == 'Pitch  Parameter':
+                self.pitch_start = float(loglines[line+1].split()[-1])
+                self.pitch_end = float(loglines[line+2].split()[-1])
+                self.pitch_step = float(loglines[line+3].split()[-1])
+                self.pitch_velocity = float(loglines[line+5].split()[-1])
+                self.pitch_return_velocity = float(loglines[line+6].split()[-1])
+                self.pitch_unit = 'deg'
