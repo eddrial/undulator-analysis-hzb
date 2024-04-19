504bd18b63050aebbd7022f9d81a28a95b093bc4
Ed Rial <eddrial@hotmail.com>
2024-04-19 17:09:46 +0200

    Learning PyQt

--- src/undulator_analysis_hzb/ua_gui.py
+++ src/undulator_analysis_hzb/ua_gui.py
@@ -3,37 +3,55 @@ Created on Apr 19, 2024
 
 @author: oqb
 '''
-from PyQt6.QtCore import QSize, Qt
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
 import sys
 
-#To create window, create classes.... could probably be outsourced
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QPalette, QColor
+from PyQt6.QtWidgets import (
+    QApplication,
+    QLabel,
+    QMainWindow,
+    QPushButton,
+    QTabWidget,
+    QWidget,
+)
+
+class Color(QWidget):
+
+    def __init__(self, color):
+        super(Color, self).__init__()
+        self.setAutoFillBackground(True)
+
+        palette = self.palette()
+        palette.setColor(QPalette.ColorRole.Window, QColor(color))
+        self.setPalette(palette)
+
+# Subclass QMainWindow to customize your application's main window
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        
-        self.setWindowTitle('Undulator Analysis HZB')
-        
-        button = QPushButton('Go on then!')
-        
-        #self.setFixedSize(QSize(400,300))
-        
-        self.setMinimumSize(QSize(100, 100))
-        self.setMaximumSize(QSize(400,400))
-        
-        
-        
-        self.setCentralWidget(button)
 
+        self.setWindowTitle("My App")
 
+        tabs = QTabWidget()
+        tabs.setTabPosition(QTabWidget.TabPosition.South)
+        tabs.setMovable(True)
 
+        for n, color in enumerate(["red", "green", "blue", "yellow"]):
+            tabs.addTab(Color(color), color)
+
+        self.setCentralWidget(tabs)
 #This bit is the running of the app
 app = QApplication(sys.argv)
 
+#create the main window
 window = MainWindow()
 
+#show the window
 window.show()
 
+#begin the action loop
 app.exec()
 
-print('I am here')
+#once closed, you're outta here
+print('I am here https://www.pythonguis.com/tutorials/pyqt6-layouts/')
